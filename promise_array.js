787fc207579cfa9abf0308f65a3efa84fa694cde
Louis <huynguyennbk@gmail.com>
2021-10-03 16:32:15 +0700

    promise map util

---
+++ promise_array.js
@@ -0,0 +1,11 @@
+if (!Promise.map) {
+  Promise.map = function (vals, cb) {
+    return Promise.all(
+      vals.map(function (val) {
+        return new Promise(function (resolve) {
+          cb(val, resolve);
+        });
+      })
+    );
+  };
+}
